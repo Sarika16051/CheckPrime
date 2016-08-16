bab00ea68fb21810ce668e774c41d7a773c22bab
sarika <sarika16051@iiitd.ac.in>
2016-08-16 15:43:53 +0530

    Assignment1- 1st attempt

---
+++ app/src/main/java/com/example/sarika/checkprime/MainActivity.java
@@ -0,0 +1,137 @@
+package com.example.sarika.checkprime;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private static final String TAG = "MainActivity";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView question = (TextView) findViewById(R.id.text_question);
+        /* Get the value from the saved instance if instance saved and displaying the question */
+        if (savedInstanceState != null) {
+            int val = savedInstanceState.getInt(TAG);
+            question.setText(val + " is a prime number");
+        } else {
+            Random r1 = new Random();
+            int val = r1.nextInt(1000 - 1) + 1;
+            question.setText(val + " is a prime number");
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /* Saving the instance */
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        TextView question = (TextView) findViewById(R.id.text_question);
+        String ques = question.getText().toString();
+        String quest[] = ques.split(" ");
+        int val = Integer.parseInt(quest[0]);
+        savedInstanceState.putInt(TAG, val);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    /* Displays Correct if the number is not prime and Wrong if the number is prime through the use of a toast */
+    public void CheckFalse(View view) {
+        TextView question = (TextView) findViewById(R.id.text_question);
+        String ques = (String) question.getText();
+        String quest[] = ques.split(" ");
+        int value = Integer.parseInt(quest[0]);
+        int c = Check(value);
+        view.requestFocus();
+        if (c == 0) {
+            Toast.makeText(this, "Correct", Toast.LENGTH_SHORT).show();
+        } else if (c == 1) {
+            Toast.makeText(this, "Wrong", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "1 is neither a prime nor a composite number ", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    /* Displays Correct if the number is prime and Wrong if the number is not prime through the use of a toast */
+    public void CheckTrue(View view) {
+        TextView question = (TextView) findViewById(R.id.text_question);
+        String ques = (String) question.getText();
+        String quest[] = ques.split(" ");
+        int value = Integer.parseInt(quest[0]);
+        int c = Check(value);
+        view.requestFocus();
+        if (c == 1) {
+            Toast.makeText(this, "Correct", Toast.LENGTH_SHORT).show();
+        } else if (c == 0) {
+            Toast.makeText(this, "Wrong", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "1 is neither a prime nor a composite number ", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    /* Sets the TextView to display a new question */
+    public void GoNext(View view) {
+        TextView question = (TextView) findViewById(R.id.text_question);
+        Random r1 = new Random();
+        int val = r1.nextInt(1000 - 1) + 1;
+        question.setText(val + " is a prime number");
+        view.requestFocus();
+    }
+
+    /* Returns integer value 0 if the passed argument is not prime and returns 1 if prime */
+    private int Check(int check) {
+        int ans = 1;
+        if (check == 1) {
+            ans = 3;
+            return ans;
+        }
+        for (int j = 2; j < check / 2; j++) {
+            if ((check % j) == 0) {
+                ans = 0;//is not prime
+                return ans;
+            } else {
+                ans = 1;//is prime
+            }
+
+        }
+        return ans;
+    }
+
+
+}
+
